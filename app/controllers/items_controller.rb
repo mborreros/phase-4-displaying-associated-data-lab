e22277691134f1a5f58f3b9be0c9b4d048861462
mborreros <mlbsindia@gmail.com>
2022-06-07 16:55:41 -0400

    commit tree b56489ffa2d218fcd0afc5806f3312854a5654a0

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,8 @@
 class ItemsController < ApplicationController
+
+  def index
+    all_items = Item.all
+    render json: all_items, include: :user
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+
+  def show
+    this_user = User.find(params[:id])
+    render json: this_user, include: :items
+  end
+
 end
